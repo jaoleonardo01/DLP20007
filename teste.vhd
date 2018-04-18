d04dcb5d7cc5655e4cac393f3c614cd2e68efd81
Aluno <jao@oldschool.not.br>
2018-04-18 09:21:20 -0300

    teste

--- teste.vhd
+++ teste.vhd
@@ -6,13 +6,13 @@ entity teste is
 
     generic
     (
-        DATA_WIDTH : natural := 8
+        DATA_WIDTH : natural := 32
     );
 
     port
     (
-        e1,e2    : in std_logic_vector((DATA_WIDTH-1) downto 0);
-        e3    : in std_logic;
+        e1,e2,e3,e4,e5,e6,e7,e8    : in std_logic_vector((DATA_WIDTH-1) downto 0);
+        z3    : in std_logic;
        
         q        : out std_logic_vector((DATA_WIDTH-1) downto 0)
     );
@@ -24,7 +24,7 @@ architecture v1 of teste is
 begin
 
     q<=
-        e1 when e3='1' else
+        e1 when z3='1' else
         e2;
    
 end v1;
@@ -52,7 +52,47 @@ begin
    
 end v3;
 
+architecture v4 of teste is --aula do dia 18-04 - somador paralelo e serial
+
+    signal sum1, sum2, sum3, sum4, sum5, sum6, sum7, sum8: signed ((DATA_WIDTH-1) downto 0);
+
+begin
+
+    sum1<=signed(e1);
+    sum2<=signed(e2);
+	 sum3<=signed(e3);
+	 sum4<=signed(e4);
+	 sum5<=signed(e5);
+	 sum6<=signed(e6);
+	 sum7<=signed(e7);
+	 sum8<=signed(e8);
+   
+    q<=
+        std_logic_vector(((((((sum1 + sum2) + sum3) + sum4) + sum5) + sum6) + sum7) + sum8);
+   
+end v4;
+
+architecture v5 of teste is --aula do dia 18-04 - somador paralelo e serial
+
+    signal sum1, sum2, sum3, sum4, sum5, sum6, sum7, sum8: signed ((DATA_WIDTH-1) downto 0);
+
+begin
+
+    sum1<=signed(e1);
+    sum2<=signed(e2);
+	 sum3<=signed(e3);
+	 sum4<=signed(e4);
+	 sum5<=signed(e5);
+	 sum6<=signed(e6);
+	 sum7<=signed(e7);
+	 sum8<=signed(e8);
+   
+    q<=
+        std_logic_vector(((sum1 + sum2) + (sum3 + sum4)) + ((sum5 + sum6) + (sum7 + sum8)));
+   
+end v5;
+
 configuration cfg of teste is  -- Configuration Declaration
-    for v1                     -- Block Configuration
+    for v5                     -- Block Configuration
     end for;
 end cfg;
